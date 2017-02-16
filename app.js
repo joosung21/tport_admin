9341ae835ce5d2da89a0db7fc69dd082322998bc
Jay Ahn <joosung21@gmail.com>
2017-02-16 22:06:49 +0900

    Start project

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express()
+var pug = require('pug')
+app.set('views', './views')
+app.set('view engine', 'pug')
+app.locals.pretty = true
+app.use('/assets', express.static('assets'))
+app.use('/lang', express.static('lang'))
+app.use('/', express.static('public'))
+
+app.get('/tport/login', function (req, res) {
+  res.render('tport_login')
+})
+app.get('/tport/login/set-pw', function (req, res) {
+  res.render('tport_login_setpw')
+})
+app.get('/tport/members', function (req, res) {
+  res.render( 'tport_members',{
+    pageId: 'tport_members',
+  })
+})
+
+// SERVER LISTNER
+app.listen(3000, function () {
+  console.log('todolist app listening on port 3000!')
+})
---
+++ assets/pages/scripts/dashboard.js
@@ -0,0 +1,166 @@
+var Dashboard = function() {
+
+    return {
+
+        initCharts: function() {
+            if (!jQuery.plot) {
+                return;
+            }
+
+            function showChartTooltip(x, y, xValue, yValue) {
+                $('<div id="tooltip" class="chart-tooltip">' + yValue + '<\/div>').css({
+                    position: 'absolute',
+                    display: 'none',
+                    top: y - 40,
+                    left: x - 40,
+                    border: '0px solid #ccc',
+                    padding: '2px 6px',
+                    'background-color': '#fff'
+                }).appendTo("body").fadeIn(200);
+            }
+
+            var data = [];
+            var totalPoints = 250;
+
+            // random data generator for plot charts
+
+            function getRandomData() {
+                if (data.length > 0) data = data.slice(1);
+                // do a random walk
+                while (data.length < totalPoints) {
+                    var prev = data.length > 0 ? data[data.length - 1] : 50;
+                    var y = prev + Math.random() * 10 - 5;
+                    if (y < 0) y = 0;
+                    if (y > 100) y = 100;
+                    data.push(y);
+                }
+                // zip the generated y values with the x values
+                var res = [];
+                for (var i = 0; i < data.length; ++i) res.push([i, data[i]])
+                return res;
+            }
+
+            function randValue() {
+                return (Math.floor(Math.random() * (1 + 50 - 20))) + 10;
+            }
+
+            if ($('#site_activities').size() != 0) {
+                //site activities
+                var previousPoint2 = null;
+                $('#site_activities_loading').hide();
+                $('#site_activities_content').show();
+
+                var data1 = [
+                    ['2016/12', 300],
+                    ['2017/1', 600],
+                    ['2017/2', 1100],
+                    ['2017/3', 1200],
+                    ['2017/4', 860],
+                    ['2017/5', 1200],
+                    ['2017/6', 1450],
+                    ['2017/7', 1800],
+                    ['2017/8', 1200],
+                    ['2017/9', 600]
+                ];
+
+
+                var plot_statistics = $.plot($("#site_activities"),
+
+                    [{
+                        data: data1,
+                        lines: {
+                            fill: 0.2,
+                            lineWidth: 0,
+                        },
+                        color: ['#BAD9F5']
+                    }, {
+                        data: data1,
+                        points: {
+                            show: true,
+                            fill: true,
+                            radius: 4,
+                            fillColor: "#9ACAE6",
+                            lineWidth: 2
+                        },
+                        color: '#9ACAE6',
+                        shadowSize: 1
+                    }, {
+                        data: data1,
+                        lines: {
+                            show: true,
+                            fill: false,
+                            lineWidth: 3
+                        },
+                        color: '#9ACAE6',
+                        shadowSize: 0
+                    }],
+
+                    {
+
+                        xaxis: {
+                            tickLength: 0,
+                            tickDecimals: 0,
+                            mode: "categories",
+                            min: 0,
+                            font: {
+                                lineHeight: 18,
+                                style: "normal",
+                                variant: "small-caps",
+                                color: "#6F7B8A"
+                            }
+                        },
+                        yaxis: {
+                            ticks: 5,
+                            tickDecimals: 0,
+                            tickColor: "#eee",
+                            font: {
+                                lineHeight: 14,
+                                style: "normal",
+                                variant: "small-caps",
+                                color: "#6F7B8A"
+                            }
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true,
+                            tickColor: "#eee",
+                            borderColor: "#eee",
+                            borderWidth: 1
+                        }
+                    });
+
+                $("#site_activities").bind("plothover", function(event, pos, item) {
+                    $("#x").text(pos.x.toFixed(2));
+                    $("#y").text(pos.y.toFixed(2));
+                    if (item) {
+                        if (previousPoint2 != item.dataIndex) {
+                            previousPoint2 = item.dataIndex;
+                            $("#tooltip").remove();
+                            var x = item.datapoint[0].toFixed(2),
+                                y = item.datapoint[1].toFixed(2);
+                            showChartTooltip(item.pageX, item.pageY, item.datapoint[0], item.datapoint[1] + '만원');
+                        }
+                    }
+                });
+
+                $('#site_activities').bind("mouseleave", function() {
+                    $("#tooltip").remove();
+                });
+            }
+        },
+
+        init: function() {
+
+
+            this.initCharts();
+
+        }
+    };
+
+}();
+
+if (App.isAngularJsApp() === false) {
+    jQuery(document).ready(function() {
+        Dashboard.init(); // init metronic core componets
+    });
+}
